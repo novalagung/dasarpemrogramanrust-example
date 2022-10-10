9542aa6f7e2f1ddae754f5f056239536a9019d16
novalagung <caknopal@gmail.com>
2022-10-10 15:54:41 +0700

    feat: update string sample

--- tipe_data_string_literal/src/main.rs
+++ tipe_data_string_literal/src/main.rs
@@ -1,4 +1,37 @@
 fn main() {
-    let var1 = "hello";
-    println!("{}", var1);
+    // let var1 = "hello";
+    // println!("{}", var1);
+
+    let var2 = "hello \
+        \"rust\" \
+        and \
+        \"world\"";
+    println!("{}", var2);
+
+//     let var3 = "baris satu
+// baris dua
+// baris tiga";
+//     println!("{}", var3);
+
+    // let var4 = "baris satu
+    //     baris dua
+    //     baris tiga
+    // ";
+    // println!("{}", var4);
+
+    // let var5 = r#"
+    // {
+    //     "name": "tim drake",
+    //     "gender": "male"
+    // }
+    // "#;
+    // println!("{}", var5);
+
+    // let var6 = "
+    // {
+    //     \"name\": \"cassandra cain\",
+    //     \"gender\": \"female\"
+    // }
+    // ";
+    // println!("{}", var6);
 }
