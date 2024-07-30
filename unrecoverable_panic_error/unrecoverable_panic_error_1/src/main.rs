672a2e41a17b78ba70e212652e114b228d458d7a
novalagung <hello@novalagung.com>
2024-07-30 16:55:34 +0700

    feat: add unrecoverable error

---
+++ unrecoverable_panic_error/unrecoverable_panic_error_1/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let data = [10, 11, 23];
+    // let data = vec![10, 11, 23];
+
+    println!("data index 1: {}", data[1]);
+    println!("data index 6: {}", data[6]);
+    println!("data index 2: {}", data[2]);
+}
