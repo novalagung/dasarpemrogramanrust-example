53e068ebf6cf58ddc1e06f40e617257a89062673
novalagung <hello@novalagung.com>
2026-05-10 22:09:14 +0700

    feat: add orphan rule & local trait

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -5,13 +5,31 @@ fn main() {
     // let text = String::from("hello");
     // println!("{:?}", text);
 
-    let circle_one = Circle{radius: 6};
+    let circle_one = Circle { radius: 6 };
     println!("{:?}", circle_one);
     println!("{}", circle_one);
+
+    let wrapped = Wrapper(vec![String::from("a"), String::from("b")]);
+    println!("{}", wrapped);
+
+    let s = String::from("hello");
+    s.log();
 }
 
 struct Circle {
     radius: i32,
+}
+
+struct Wrapper(Vec<String>);
+
+trait Message {
+    fn log(&self);
+}
+
+impl Message for String {
+    fn log(&self) {
+        println!("{}", self);
+    }
 }
 
 impl std::fmt::Debug for Circle {
@@ -23,5 +41,11 @@ impl std::fmt::Debug for Circle {
 impl std::fmt::Display for Circle {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Circle radius: {}", self.radius)
+    }
+}
+
+impl std::fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.0)
     }
 }
